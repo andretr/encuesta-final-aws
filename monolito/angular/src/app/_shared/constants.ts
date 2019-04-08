1c72fc0f6c9055f6008d25948666a1b68220e9c4
andre <andre.ticona.r@gmail.com>
2019-04-08 00:55:14 -0400

    version final

--- monolito/angular/src/app/_shared/constants.ts
+++ monolito/angular/src/app/_shared/constants.ts
@@ -1,6 +1,7 @@
 //export const HOST_BACKEND = `http://localhost:8080`
 //  export const HOST_BACKEND = `http://192.168.99.100`
-export const HOST_BACKEND = `http://balancerfinalaws-436644537.us-east-1.elb.amazonaws.com`
+// export const HOST_BACKEND = `http://balancerfinalaws-436644537.us-east-1.elb.amazonaws.com`
+export const HOST_BACKEND = `https://8uk9jvclxe.execute-api.us-east-1.amazonaws.com/prod`
 
 export const TOKEN_NAME = "idToken";
 export const REFRESH_TOKEN_NAME = "refreshToken";
